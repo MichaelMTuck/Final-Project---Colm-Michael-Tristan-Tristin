02f5d0023ae01331b46d97c9ec208085b4b95fb9
TremsOrbit <thetertiary606@gmail.com>
2025-12-05 15:43:31 -0500

    all assembly program tests are passing - TH

--- asm/multiply_p2.asm
+++ asm/multiply_p2.asm
@@ -1,12 +1,19 @@
-; multiply_p2.asm - TH
-; multiply 3 by powers of two, store in R0-R7
+; multiply_p2.asm - golden version TH
 
-LOADI R0, #3        ; original constant
-LOADI R1, #1        ; will hold shift amount 1
-SHFT R2, R0, R1     ; 3 * 2^1 = 6
-SHFT R3, R2, R1     ; 6 * 2^1 = 12
-SHFT R4, R3, R1     ; 12 * 2^1 = 24
-SHFT R5, R4, R1     ; 24 * 2^1 = 48
-SHFT R6, R5, R1     ; 48 * 2^1 = 96
-SHFT R7, R6, R1     ; 96 * 2^1 = 192
+LOADI R0 3       ; original constant
+LOADI R1 1       ; shift amount (multiply by 2 each time)
+
+SHFT R2 R0 R1    ; 3 * 2 = 6
+SHFT R3 R2 R1    ; 6 * 2 = 12
+SHFT R4 R3 R1    ; 12 * 2 = 24
+SHFT R5 R4 R1    ; 24 * 2 = 48
+SHFT R6 R5 R1    ; 48 * 2 = 96
+SHFT R7 R6 R1    ; 96 * 2 = 192
+
+ADDI R0 R0 0
+ADDI R1 R1 5
+ADDI R2 R2 0
+ADDI R3 R3 0
+ADDI R4 R4 0
+
 HALT
